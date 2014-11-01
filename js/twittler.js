b618b45db2bb7b7bbd969ebaed53978e8fea3039
Todd Skinner <todd.skinner@gmail.com>
2014-10-31 17:30:09 -0700

    Add ability to click on user and go back to home stream and add initial CSS

--- js/twittler.js
+++ js/twittler.js
@@ -2,12 +2,19 @@ $(document).ready(function(){
   var $body = $('.tweets');          //people sometimes use a $ with jQuery-specific variables is that it helps them remember that the variable contains the result of a call to the $() function in jQuery. 
   $body.html('');
 
+  var stream = streams.home;
+
   var showTweets = function(){
-    var index = streams.home.length - 1;      //streams.home is an array of all tweets from all users you are following
+    var index = stream.length - 1;      //streams.home is an array of all tweets from all users you are following
     while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div></div>');          //$('<div></div> will create a div element but won't add it to your HTML doc. Need to appeand later.)
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ": " + tweet.created_at);
+      var tweet = stream[index];
+      var $tweet = $('<div class="singleTweet"></div>');          //$('<div></div> will create a div element but won't add it to your HTML doc. Need to appeand later.)
+      //$tweet.text('@' + tweet.user + ': ' + tweet.message + ": " + tweet.created_at);
+      //$tweet.appendTo('.tweets');
+      var name = '<b>' + tweet.user + '</b>';
+      var msg = ' <small><a class="user" data-user="'+ tweet.user +'" href="#">@' + tweet.user + '</a></small>: ' + tweet.message;
+      var time = '<br>' + '<small>' + tweet.created_at + '</small>';
+      $tweet.html(name + msg + time);
       $tweet.appendTo('.tweets');
       index -= 1;
     }
@@ -17,13 +24,24 @@ $(document).ready(function(){
 
   setInterval(function(){
     showTweets();
-  }, 2000);
+  }, 5000);
 
   var changeStream = function(newStream){
-    $('tweets').html('');
+    $('.tweets').html('');
     stream = newStream;
     showTweets();
   }
 
+  $('.backHome').click(function(){
+    changeStream(streams.home);
+    $('h1').html('Home');
+  });
     
+  $('.tweets').on('click', 'a.user', function(event){
+    event.preventDefault();
+    var username = $(this).data('user');
+    $('h1').html(username);
+    changeStream(streams.users[username]);
+  });
+
 });
